f9dd85eb4c73f94bbbb1e08be5ddf68b5f80ed88
Stanley <stanley.traub@gmail.com>
2020-05-31 10:01:07 -0700

    Finish nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Home from './pages/Home'
+import Nav from './components/nav'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <Nav/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Nav = () => (
-    <div>
-    </div>
-)
+  <nav className="nav-bar">
+    <ul className="nav-bar__links flex-row">
+      <li className="nav-bar__links__link">HOME</li>
+      <li className="nav-bar__links__link">ABOUT</li>
+      <li className="nav-bar__links__link">PORTFOLIO</li>
+      <li className="nav-bar__links__link">CONTACT</li>
+    </ul>
+  </nav>
+);
 
 export default Nav
